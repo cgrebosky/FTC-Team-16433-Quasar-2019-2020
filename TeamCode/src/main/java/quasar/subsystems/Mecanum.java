c798b43717bfeacd8f6519e189f4975c0a046f55
Carson Grebosky <cgrebosky@gmail.com>
2019-10-11 14:39:00 -0700

    Fix Mecanum subsystem - It actually works now!!! :D
    Signed-off-by: Carson Grebosky <cgrebosky@gmail.com>

--- TeamCode/src/main/java/quasar/subsystems/Mecanum.java
+++ TeamCode/src/main/java/quasar/subsystems/Mecanum.java
@@ -1,14 +1,7 @@
 package quasar.subsystems;
 
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.LinkedList;
 
 import quasar.lib.MoreMath;
 import quasar.lib.SubSystem;
@@ -19,10 +12,15 @@ public class Mecanum extends SubSystem {
 
     private double THRESHOLD = 0.1;
 
+    private double left, fwd, rot;
+
     //These are the coefficients for the motorpowers in order of fl, fr, bl, br (in order as defined above)
-    private final double[] FWD       = {1,-1,-1,1};
-    private final double[] LEFT      = {-1,-1,-1,-1};
-    private final double[] CLOCKWISE = {-1,1,-1,1};
+    private final double[] FWD       = {1,1,1,1};
+    private final double[] STRAFE      = {1,-1,-1,1};
+    private final double[] TURN = {1,-1,1,-1};
+
+    double turnCorrection = 0.0;
+
     private       double[] powers    = {0, 0, 0,0};
 
     @Override
@@ -32,32 +30,66 @@ public class Mecanum extends SubSystem {
         bl = hardwareMap.dcMotor.get("bl");
         br = hardwareMap.dcMotor.get("br");
 
+        bl.setDirection(DcMotorSimple.Direction.REVERSE);
+        fl.setDirection(DcMotorSimple.Direction.REVERSE);
+
         fl.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         fr.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         bl.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         br.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        fl.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        fr.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        bl.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        br.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+
     }
+
     @Override
     public void loop() {
+
+        if(gamepad1.a) turnCorrection+=0.01;
+        if(gamepad1.b) turnCorrection-=0.01;
+
+        zeroControls();
         calculatePowers();
         normalizeMotorPowers();
-        zeroPowers();
         setMotorPowers();
 
-        opm.telemetry.addLine(powers[0] + ", " + powers[1] + ", " + powers[2] + ", " + powers[3]);
-        opm.telemetry.addLine("" + gamepad1.left_stick_y);
+        opm.telemetry.addData("FL", powers[0]);
+        opm.telemetry.addData("FR", powers[1]);
+        opm.telemetry.addData("BL", powers[2]);
+        opm.telemetry.addData("BR", powers[3]);
+        opm.telemetry.addLine();
+        opm.telemetry.addData("Forward", fwd);
+        opm.telemetry.addData("Left", left);
+        opm.telemetry.addData("Rotation", rot);
+        opm.telemetry.addLine();
+        opm.telemetry.addData("Turn Correction", turnCorrection);
+
+
     }
+
 
     private void calculatePowers() {
         //Why tf doesn't java have list stuff automatically???  Why do I have to make this stuff manually?? >:(
-        powers = MoreMath.listMultiply(gamepad1.left_stick_y, FWD);
-        powers = MoreMath.listAdd(powers, MoreMath.listMultiply(gamepad1.left_stick_x, LEFT));
-        powers = MoreMath.listAdd(powers, MoreMath.listMultiply(gamepad1.right_stick_x, CLOCKWISE));
+        powers = MoreMath.listMultiply(fwd, FWD);
+        powers = MoreMath.listAdd(powers, MoreMath.listMultiply(left, STRAFE));
+        powers = MoreMath.listAdd(powers, MoreMath.listMultiply(rot, TURN));
+
+        double[] corr = MoreMath.listMultiply(turnCorrection, MoreMath.listMultiply(gamepad1.left_stick_x, FWD));
+
+        powers = MoreMath.listAdd(powers, corr);
     }
-    private void zeroPowers() {
-        for (int i = 0; i < 4; i++) {
-            if(Math.abs(powers[i]) < THRESHOLD) powers[i] = 0;
-        }
+    private void zeroControls() {
+        left = gamepad1.left_stick_x;
+        fwd = -gamepad1.left_stick_y;
+        rot = gamepad1.right_stick_x;
+
+        left = (Math.abs(left)<THRESHOLD)?0:left;
+        fwd = (Math.abs(fwd)<THRESHOLD)?0:fwd;
+        rot = (Math.abs(rot)<THRESHOLD)?0:rot;
     }
     private void setMotorPowers() {
         fl.setPower(powers[0]);
@@ -69,7 +101,7 @@ public class Mecanum extends SubSystem {
 
         double max = 1;
         for (double i : powers) {
-            if(Math.abs(i) > max) max = i;
+            if(Math.abs(i) > Math.abs(max)) max = Math.abs(i);
         }
 
         powers = MoreMath.listMultiply(1/max, powers);
