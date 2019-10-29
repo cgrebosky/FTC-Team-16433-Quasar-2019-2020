ae988a6ec73ff8dbfff892050d11f1fe8d75cef5
cgrebosky <cgrebosky@gmail.com>
2019-10-28 18:15:49 -0700

    Update MotorTester code to allow for more diverse code
    Signed-off-by: Carson Grebosky <cgrebosky@gmail.com>

--- tools/MotorTester.java
+++ tools/MotorTester.java
@@ -3,18 +3,28 @@ package quasar.tools;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 @TeleOp(name = "Motor Tester", group = "Tools")
 public class MotorTester extends OpMode {
 
     private DcMotor motor;
 
+    private double position; //[0,250]
+
     @Override
     public void init() {
         motor = hardwareMap.dcMotor.get("motor");
+
+        motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        motor.setTargetPosition(0);
+        motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
     }
     @Override
     public void loop() {
+        motor.setTargetPosition((int) position);
+        motor.setPower(0.7);
+
         telemetry.addData("Position", motor.getCurrentPosition());
         telemetry.addData("Direction", motor.getDirection());
         telemetry.addData("Mode", motor.getMode());
@@ -23,6 +33,9 @@ public class MotorTester extends OpMode {
         telemetry.addData("Target Position",motor.getTargetPosition());
         telemetry.addData("Zero Power Behaviour", motor.getZeroPowerBehavior());
         telemetry.update();
+
+        position += gamepad1.left_trigger;
+        position -= gamepad1.right_trigger;
     }
     @Override
     public void stop() {
