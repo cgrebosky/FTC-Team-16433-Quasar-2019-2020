ac2557203dcd45c38b9e49ca633e7bdc0fc7faaf
Carson Grebosky <cgrebosky@gmail.com>
2020-02-23 17:23:26 -0800

    Get rid of unused methods in MoreMath
    Signed-off-by: Carson Grebosky <cgrebosky@gmail.com>

--- lib/MoreMath.java
+++ lib/MoreMath.java
@@ -1,30 +1,6 @@
 package quasar.lib;
 
 public class MoreMath {
-    //Represents scalar * vector multiplication
-    public static double[] listMultiply(double a, double[] b) {
-        double[] product = b.clone();
-
-        for (int i = 0; i < b.length; i++) {
-            product[i] *= a;
-        }
-
-        return product;
-    }
-    //Represents vector + vector addition
-    public static double[] listAdd(double[] a, double[] b) {
-        if(a.length != b.length) {
-            throw new IllegalArgumentException("Array lengths must be the same!  Currently a is "
-                    + a.length + " and b is " + b.length);
-        }
-
-        double[] prod = a.clone();
-        for (int i = 0; i < a.length; i++) {
-            prod[i] = a[i] + b[i];
-        }
-        return prod;
-    }
-
     /**
      * Linear interpolation
      * @param a Point 1
