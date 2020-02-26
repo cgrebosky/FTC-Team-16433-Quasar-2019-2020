cb8c9125c8cce0461a071d83e87e6f770f3bd90d
Carson Grebosky <cgrebosky@gmail.com>
2020-02-25 20:06:21 -0800

    Comment on AutoTransitioner
    Signed-off-by: Carson Grebosky <cgrebosky@gmail.com>

--- lib/AutoTransitioner.java
+++ lib/AutoTransitioner.java
@@ -8,14 +8,8 @@ import org.firstinspires.ftc.robotcontroller.internal.FtcRobotControllerActivity
 import org.firstinspires.ftc.robotcore.internal.opmode.OpModeManagerImpl;
 
 /**
- * Created by KNO3 Robotics
- * AutoTransitioner is a utility to automatically initialize the teleop program of your choice
- * after the autonomous period ends. To use AutoTransitioner, go to your OpMode/LinearOpMode,
- * and place the following line of code in your init() method or before your waitForStart()
- * (for OpMode and LinearOpMode, respectively):
- *      AutoTransitioner.transitionOnStop(this, "Robot Teleop");
- * Where 'Robot Teleop' is replaced with the NAME of your teleop program. See full documentation
- * on kno3.net/resources for more info.
+ * This file was created by KNO3 Robotics, a now defunct FTC team.  They will be missed.  It was modified
+ * slightly by Quasar to suit our nefarious purposes >:D.  For KNO3's version, see their website, kno3.net
  */
 public class AutoTransitioner extends Thread {
     private static final AutoTransitioner INSTANCE = new AutoTransitioner(); //Create singleton instance
