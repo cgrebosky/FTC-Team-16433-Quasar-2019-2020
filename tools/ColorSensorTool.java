f09e1a11a7ee80fc9ca6d8369f3d9f5d826a0158
Carson Grebosky <cgrebosky@gmail.com>
2020-02-26 17:30:05 -0800

    Add some stuff to ColorSensorTool
    Signed-off-by: Carson Grebosky <cgrebosky@gmail.com>

--- tools/ColorSensorTool.java
+++ tools/ColorSensorTool.java
@@ -1,5 +1,7 @@
 package quasar.tools;
 
+import android.graphics.Color;
+
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -15,20 +17,26 @@ import static org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit.C
 public final class ColorSensorTool extends OpMode {
 
     ColorSensor left, right;
-    DistanceSensor distance;
+    DistanceSensor distance, leftDist, rightDist;
 
     @Override
     public void init() {
         left = hardwareMap.colorSensor.get("colorLeft");
         right = hardwareMap.colorSensor.get("colorRight");
 
+        leftDist = hardwareMap.get(DistanceSensor.class, "colorLeft");
+        rightDist = hardwareMap.get(DistanceSensor.class, "colorRight");
+
         distance = hardwareMap.get(DistanceSensor.class, "distance");
     }
 
     @Override
     public void loop() {
+        //40- = black, 40-50 = nothing, 50+ = yellow
         telemetry.addData("Left RGB", left.red() + ", " + left.green() + ", " + left.blue());
+        telemetry.addData("Left Distance", leftDist.getDistance(CM));
         telemetry.addData("Right RGB", right.red() + ", " + right.green() + ", " + right.blue());
+        telemetry.addData("Right Distance", rightDist.getDistance(CM));
         telemetry.addData("Distance (CM)", distance.getDistance(CM));
         telemetry.update();
     }
