7b638eef87b34278c48e9b1d5ec6310b8bc48cad
Carson Grebosky <cgrebosky@gmail.com>
2020-02-27 17:42:51 -0800

    Create the general Robot system to encapsulate all our subsystems :D
    Signed-off-by: Carson Grebosky <cgrebosky@gmail.com>

--- prod/QuasarTeleOp.java
+++ prod/QuasarTeleOp.java
@@ -14,44 +14,19 @@ import quasar.subsystems.PlatformMover;
 @TeleOp(name = "Quasar TeleOp", group = "Prod")
 public class QuasarTeleOp extends OpMode {
 
-    private Collector c     = new Collector();
-    private Lift l          = new Lift();
-    private Mecanum m       = new Mecanum();
-    private PlatformMover p = new PlatformMover();
-    private CapstoneDepositor cd = new CapstoneDepositor();
-
     @Override
     public void init() {
-        c.create(this);
-        l.create(this);
-        m.create(this);
-        p.create(this);
-        cd.create(this);
-
-        c.init();
-        l.init();
-        m.init();
-        p.init();
-        cd.init();
+        Robot.create(this);
+        Robot.init();
     }
 
     @Override
     public void loop() {
-        c.loop();
-        l.loop();
-        m.loop();
-        p.loop();
-        cd.loop();
-
-        telemetry.update();
+        Robot.loop();
     }
 
     @Override
     public void stop() {
-        c.stop();
-        l.stop();
-        m.stop();
-        p.stop();
-        cd.stop();
+        Robot.stop();
     }
 }
--- prod/Robot.java
+++ prod/Robot.java
@@ -3,37 +3,97 @@ package quasar.prod;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
-import org.firstinspires.ftc.robotcore.internal.opmode.OpModeManagerImpl;
-
+import quasar.lib.macro.MacroState;
+import quasar.lib.macro.MacroSystem;
 import quasar.subsystems.*;
 import quasar.subsystems.threaded.IMUHandler;
 import quasar.subsystems.threaded.VuforiaPositionDetector;
 
-public final class Robot {
-    public static Robot INSTANCE = new Robot();
+public final class Robot implements MacroSystem {
+    private static Collector co = new Collector();
+    private static Lift li = new Lift();
+    private static Mecanum me = new Mecanum();
+    private static PlatformMover pm = new PlatformMover();
+    private static CapstoneDepositor cp = new CapstoneDepositor();
     
-    public Collector co;
-    public Lift li;
-    public Mecanum me;
-    public PlatformMover pm;
-    
-    public IMUHandler imu;
-    public VuforiaPositionDetector vpd;
+    private static IMUHandler imu = new IMUHandler();
+    private static VuforiaPositionDetector vpd = new VuforiaPositionDetector();
 
-    private LinearOpMode lop;
-    private OpMode opm;
-    private OpModeManagerImpl manager;
+    private static LinearOpMode lop = null;
+    private static OpMode opm = null;
 
-
-    public void create() {
-
+    public static void create(LinearOpMode lop) {
+        Robot.lop = lop;
+        create((OpMode) lop);
     }
-    public void init() {
+    public static void create(OpMode opm) {
+        Robot.opm = opm;
 
-        co.create(manager.getActiveOpMode());
+        co.create(opm);
+        li.create(opm);
+        me.create(opm);
+        pm.create(opm);
+        cp.create(opm);
+    }
+
+    public static void autoInit() {
+        imu.create(lop, false);
+        say("IMU initialized");
+
+        vpd.create(lop, false);
+        say("Vuforia Positioner initialized");
+
+        init();
+    }
+    public static void init() {
         co.init();
-    }
-    public void loop() {
+        say("Collector initialized");
         
+        li.init();
+        say("Lift initialized");
+        
+        me.init();
+        say("Mecanum initialized");
+        
+        pm.init();
+        say("Platform Mover initialized");
+        
+        cp.init();
+        say("Capstone Depositor initialized");
+    }
+    public static void loop() {
+        co.loop();
+        li.loop();
+        me.loop();
+        pm.loop();
+        cp.loop();
+        opm.telemetry.update();
+    }
+    public static void stop() {
+        co.stop();
+        li.stop();
+        me.stop();
+        pm.stop();
+        cp.stop();
+    }
+
+    private static void say(Object o) {
+        opm.telemetry.addLine(""+o);
+        opm.telemetry.update();
+    }
+
+    @Override
+    public void recordMacroState() {
+        co.recordMacroState();
+        li.recordMacroState();
+        me.recordMacroState();
+        pm.recordMacroState();
+    }
+    @Override
+    public void playMacroState(MacroState m) {
+        co.recordMacroState();
+        li.recordMacroState();
+        me.recordMacroState();
+        pm.recordMacroState();
     }
 }
